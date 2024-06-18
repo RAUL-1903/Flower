1556d9e229cee49727b8ad92820d973e997ebf22
RAUL-1903 <99377568+RAUL-1903@users.noreply.github.com>
2024-06-18 12:40:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const flower = document.querySelector('.flower');
+const hearts = document.querySelector('.hearts');
+const title = document.querySelector('.title');
+
+flower.addEventListener('animationend', () => {
+  hearts.style.opacity = 1;
+  title.style.opacity = 1;
+});
+
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
